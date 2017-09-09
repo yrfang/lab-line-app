90f9ca81e54898942da3e3d781ee2fa99f7acd3a
yrfang <fyr1116@gmail.com>
2017-09-09 19:45:48 +0800

    submit the form

---
+++ javascript/board.js
@@ -0,0 +1,52 @@
+const boardForm = document.getElementById('formContainer');
+const submitState = document.getElementById("submitState");
+
+boardForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  submitState.innerHTML = '';
+  sendBroad();
+  boardForm.reset();
+});
+
+function sendBroad() {
+  const communitySelect = document.getElementById('community_select').selectedIndex;
+  const communityId = document.getElementsByTagName("option")[communitySelect].value;
+  const text = document.getElementById('text').value;
+
+  const payload = {
+    community_id: Number(communityId),
+    text: text,
+  };
+  console.log(payload);
+
+  const myHeaders = {
+    'Authorization': 'Token bb8b103dd3035b8e2cbc0ff342ea14e669046cf8',
+    'Content-Type': 'application/json',  // send json
+  };
+
+  const request = new Request(
+    'http://backgang.herokuapp.com/bot/line/community_broadcast/',
+    {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify(payload),
+      mode: 'cors',
+    }
+  );
+  console.log(request);
+
+  fetch(request).then(response => {
+    submitState.innerHTML = "Submit successfully.";
+    setTimeout(function(){ submitState.innerHTML=""; }, 2000);
+
+    //ok 200~299
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    } else console.log(response);
+    return response;
+  }).catch(function (err) {
+    console.log(err);
+    submitState.innerHTML = "Please finish the form.";
+    setTimeout(function(){ submitState.innerHTML=""; }, 2000);
+  });
+}
